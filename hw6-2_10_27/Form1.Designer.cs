133a3310158ab9ae3b58c1b9e655cafbed4d6f3a
Hank_TUF <hck134599@gmail.com>
2023-11-07 06:45:03 +0800

    Done

--- hw6-2_10_27/Form1.Designer.cs
+++ hw6-2_10_27/Form1.Designer.cs
@@ -34,6 +34,9 @@
             OFD = new OpenFileDialog();
             NoonLabel = new Label();
             Timer1 = new System.Windows.Forms.Timer(components);
+            ExportBtn = new Button();
+            MarkLabel1 = new Label();
+            MarkLabel2 = new Label();
             SuspendLayout();
             // 
             // Box
@@ -43,8 +46,8 @@
             Box.Name = "Box";
             Box.ReadOnly = true;
             Box.ScrollBars = ScrollBars.Vertical;
-            Box.Size = new Size(276, 337);
-            Box.TabIndex = 57;
+            Box.Size = new Size(276, 306);
+            Box.TabIndex = 1;
             Box.WordWrap = false;
             // 
             // TimePicker
@@ -56,7 +59,7 @@
             TimePicker.Name = "TimePicker";
             TimePicker.ShowUpDown = true;
             TimePicker.Size = new Size(120, 24);
-            TimePicker.TabIndex = 58;
+            TimePicker.TabIndex = 2;
             // 
             // SettingLabel
             // 
@@ -65,7 +68,7 @@
             SettingLabel.Location = new Point(795, 300);
             SettingLabel.Name = "SettingLabel";
             SettingLabel.Size = new Size(105, 20);
-            SettingLabel.TabIndex = 60;
+            SettingLabel.TabIndex = 3;
             SettingLabel.Text = "設定鬧鐘時間";
             // 
             // StartBtn
@@ -74,9 +77,10 @@
             StartBtn.Location = new Point(1039, 297);
             StartBtn.Name = "StartBtn";
             StartBtn.Size = new Size(75, 26);
-            StartBtn.TabIndex = 61;
+            StartBtn.TabIndex = 4;
             StartBtn.Text = "啟動";
             StartBtn.UseVisualStyleBackColor = true;
+            StartBtn.Click += StartBtn_Click;
             // 
             // FileLabel
             // 
@@ -85,7 +89,7 @@
             FileLabel.Location = new Point(149, 300);
             FileLabel.Name = "FileLabel";
             FileLabel.Size = new Size(0, 20);
-            FileLabel.TabIndex = 62;
+            FileLabel.TabIndex = 5;
             // 
             // SelectRingBtn
             // 
@@ -93,7 +97,7 @@
             SelectRingBtn.Location = new Point(50, 296);
             SelectRingBtn.Name = "SelectRingBtn";
             SelectRingBtn.Size = new Size(84, 26);
-            SelectRingBtn.TabIndex = 63;
+            SelectRingBtn.TabIndex = 6;
             SelectRingBtn.Text = "選擇鬧鈴";
             SelectRingBtn.UseVisualStyleBackColor = true;
             SelectRingBtn.Click += SelectRingBtn_Click;
@@ -105,7 +109,7 @@
             NoonLabel.Location = new Point(1137, 185);
             NoonLabel.Name = "NoonLabel";
             NoonLabel.Size = new Size(42, 70);
-            NoonLabel.TabIndex = 64;
+            NoonLabel.TabIndex = 7;
             NoonLabel.Text = "上\r\n午";
             // 
             // Timer1
@@ -114,11 +118,45 @@
             Timer1.Interval = 200;
             Timer1.Tick += Timer1_Tick;
             // 
+            // ExportBtn
+            // 
+            ExportBtn.Font = new Font("Microsoft JhengHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            ExportBtn.Location = new Point(1182, 324);
+            ExportBtn.Name = "ExportBtn";
+            ExportBtn.Size = new Size(90, 25);
+            ExportBtn.TabIndex = 8;
+            ExportBtn.Text = "匯出記錄檔";
+            ExportBtn.UseVisualStyleBackColor = true;
+            ExportBtn.Click += ExportBtn_Click;
+            // 
+            // MarkLabel1
+            // 
+            MarkLabel1.Font = new Font("Goudy Stout", 50F, FontStyle.Bold, GraphicsUnit.Point);
+            MarkLabel1.Location = new Point(370, 45);
+            MarkLabel1.Name = "MarkLabel1";
+            MarkLabel1.Size = new Size(68, 165);
+            MarkLabel1.TabIndex = 9;
+            MarkLabel1.Text = ".\r\n.";
+            MarkLabel1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // MarkLabel2
+            // 
+            MarkLabel2.Font = new Font("Goudy Stout", 50F, FontStyle.Bold, GraphicsUnit.Point);
+            MarkLabel2.Location = new Point(740, 45);
+            MarkLabel2.Name = "MarkLabel2";
+            MarkLabel2.Size = new Size(68, 165);
+            MarkLabel2.TabIndex = 9;
+            MarkLabel2.Text = ".\r\n.";
+            MarkLabel2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1470, 361);
+            Controls.Add(MarkLabel2);
+            Controls.Add(MarkLabel1);
+            Controls.Add(ExportBtn);
             Controls.Add(NoonLabel);
             Controls.Add(SelectRingBtn);
             Controls.Add(FileLabel);
@@ -143,5 +181,8 @@
         private OpenFileDialog OFD;
         private Label NoonLabel;
         private System.Windows.Forms.Timer Timer1;
+        private Button ExportBtn;
+        private Label MarkLabel1;
+        private Label MarkLabel2;
     }
 }
--- hw6-2_10_27/Form1.cs
+++ hw6-2_10_27/Form1.cs
@@ -1,62 +1,131 @@
 using System.Globalization;
 using System.Windows.Forms;
+using System.Media;
+using System.Net.Mime;
 
 namespace hw6_2_10_27 {
-    public partial class Form1 : Form {
+	public partial class Form1 : Form {
 
+		List<int>[] nums = new List<int>[10] {
+			new List<int> { 1, 2, 3, 5, 9, 10, 14, 20, 24, 25, 29, 31, 32, 33 },
+			new List<int> { 9, 14, 24, 29 },
+			new List<int> { 1, 2, 3, 9, 14, 16, 17, 18, 20, 25, 31, 32, 33 },
+			new List<int> { 1, 2, 3, 9, 14, 16, 17, 18, 24, 29, 31, 32, 33 },
+			new List<int> { 5, 9, 10, 14, 16, 17, 18, 24, 29 },
+			new List<int> { 1, 2, 3, 5, 10, 16, 17, 18, 24, 29, 31, 32, 33 },
+			new List<int> { 1, 2, 3, 5, 10, 16, 17, 18, 20, 24, 25, 29, 31, 32, 33 },
+			new List<int> { 1, 2, 3, 9, 14, 24, 29 },
+			new List<int> { 1, 2, 3, 5, 9, 10, 14, 16, 17, 18, 20, 24, 25, 29, 31, 32, 33 },
+			new List<int> { 1, 2, 3, 5, 9, 10, 14, 16, 17, 18, 24, 29, 31, 32, 33 }
+		};
 
-        List<int>[] nums = new List<int>[10] {
-            new List<int> { 1, 2, 3, 5, 9, 10, 14, 20, 24, 25, 29, 31, 32, 33 },
-            new List<int> { 9, 14, 24, 29 },
-            new List<int> { 1, 2, 3, 9, 14, 16, 17, 18, 20, 25, 31, 32, 33 },
-            new List<int> { 1, 2, 3, 9, 14, 16, 17, 18, 24, 29, 31, 32, 33 },
-            new List<int> { 5, 9, 10, 14, 16, 17, 18, 24, 29 },
-            new List<int> { 1, 2, 3, 5, 10, 16, 17, 18, 24, 29, 31, 32, 33 },
-            new List<int> { 1, 2, 3, 5, 10, 16, 17, 18, 20, 24, 25, 29, 31, 32, 33 },
-            new List<int> { 1, 2, 3, 9, 14, 24, 29 },
-            new List<int> { 1, 2, 3, 5, 9, 10, 14, 16, 17, 18, 20, 24, 25, 29, 31, 32, 33 },
-            new List<int> { 1, 2, 3, 5, 9, 10, 14, 16, 17, 18, 24, 29, 31, 32, 33 }
-        };
+		SoundPlayer player = new();
+		WMPLib.WindowsMediaPlayer wplayer = new();
+		bool isMP3 = false;
+		PictureBox[,] moniters = new PictureBox[6, 35];
+		TimeOnly alarmTime = new();
+		bool alarmOn = false;
+		bool alarmStarted = false;
 
-        PictureBox[,] moniters = new PictureBox[6, 35];
+		void Log(string s) {
+			Box.AppendText(DateTime.Now.ToString("G") + ": " + s + "\r\n");
+		}
 
-        void SetNum(int index, int num) {
-            for (int i = 0; i < 35; i++)
-                moniters[index, i].BackColor = Color.White;
-            foreach (int i in nums[num])
-                moniters[index, i].BackColor = Color.Blue;
-        }
+		void SetNum(int index, int num) {
+			for (int i = 0; i < 35; i++)
+				moniters[index, i].BackColor = Color.White;
+			foreach (int i in nums[num])
+				moniters[index, i].BackColor = Color.Blue;
+		}
 
-        public Form1() {
-            InitializeComponent();
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 35; j++) {
-                    moniters[i, j] = new PictureBox {
-                        BorderStyle = BorderStyle.FixedSingle,
-                        BackColor = Color.White,
-                        Location = new Point(50 + 185 * i + 30 * (j % 5), 45 + 30 * (j / 5)),
-                        Name = $"Block{i}_{j}",
-                        Size = new Size(30, 30),
-                        TabIndex = 0,
-                        TabStop = false
-                    };
-                    Controls.Add(moniters[i, j]);
-                }
-            }
-        }
+		public Form1() {
+			InitializeComponent();
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 35; j++) {
+					moniters[i, j] = new PictureBox {
+						BorderStyle = BorderStyle.FixedSingle,
+						BackColor = Color.White,
+						Location = new Point(50 + 185 * i + 30 * (j % 5), 45 + 30 * (j / 5)),
+						Name = $"Block{i}_{j}",
+						Size = new Size(30, 30),
+						TabIndex = 0,
+						TabStop = false
+					};
+					Controls.Add(moniters[i, j]);
+				}
+			}
+			MarkLabel1.SendToBack();
+			MarkLabel2.SendToBack();
+		}
 
-        private void SelectRingBtn_Click(object sender, EventArgs e) {
-            OFD.Filter = "音樂檔案(*.mp3,*.wav)|*.wav,*.mp3|所有檔案(*.*)|*.*";
-            if (OFD.ShowDialog() == DialogResult.OK) {
-                FileLabel.Text = OFD.FileName;
-            }
-        }
+		private void SelectRingBtn_Click(object sender, EventArgs e) {
+			//OFD.InitialDirectory = Application.StartupPath;
+			OFD.Title = "選擇鬧鈴";
+			OFD.Filter = "音樂檔案(*.mp3;*.wav)|*.wav;*.mp3|所有檔案(*.*)|*.*";
+			if (OFD.ShowDialog() == DialogResult.OK) {
+				FileLabel.Text = OFD.FileName;
+				if (OFD.FileName.EndsWith(".mp3")) {
+					isMP3 = true;
+					wplayer.URL = OFD.FileName;
+					wplayer.controls.stop();
+				} else {
+					isMP3 = false;
+					player.SoundLocation = OFD.FileName;
+				}
+				Log("已設定鬧鈴");
+			}
+		}
 
-        private void Timer1_Tick(object sender, EventArgs e) {
-            string s = DateTime.Now.ToString("hhmmsstt");
-            for (int i = 0; i < 6; i++) {
-                SetNum(i, Convert.ToInt32(s[i].ToString()));
-            }
-        }
-    }
+		private void StartBtn_Click(object sender, EventArgs e) {
+			if (!alarmOn) {
+				// check if alarm music is selected
+				if (FileLabel.Text == "") {
+					Log("錯誤訊息!");
+					MessageBox.Show("請先設定鬧鈴音樂", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+				// set alarmTime from TimePicker
+				alarmTime = new TimeOnly(TimePicker.Value.Hour, TimePicker.Value.Minute);
+				alarmOn = true;
+				StartBtn.Text = "停止";
+				Log("已設定鬧鐘");
+			} else {
+				alarmOn = false;
+				alarmStarted = false;
+				StartBtn.Text = "啟動";
+				if (isMP3) wplayer.controls.stop();
+				else player.Stop();
+				Log("已關閉鬧鐘");
+			}
+		}
+
+		private void Timer1_Tick(object sender, EventArgs e) {
+			string s = DateTime.Now.ToString("hhmmss");
+			for (int i = 0; i < 6; i++) {
+				SetNum(i, Convert.ToInt32(s[i].ToString()));
+			}
+			NoonLabel.Text = DateTime.Now.ToString("tt").Insert(1, "\n");
+
+			if (alarmOn && !alarmStarted && alarmTime == new TimeOnly(DateTime.Now.Hour, DateTime.Now.Minute)) {
+				if (isMP3) {
+					wplayer.settings.setMode("loop", true);
+					wplayer.controls.play();
+				} else player.PlayLooping();
+				alarmStarted = true;
+				MessageBox.Show("時間到!\n該起床囉~", "鬧鈴", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private void ExportBtn_Click(object sender, EventArgs e) {
+			//SFD.InitialDirectory = Application.StartupPath;
+			SFD.Title = "匯出記錄檔";
+			SFD.Filter = "文字檔案(*.txt)|*.txt";
+			if (SFD.ShowDialog() == DialogResult.OK) {
+				StreamWriter sw = new(SFD.FileName);
+				sw.Write(Box.Text);
+				sw.Close();
+				Log("已匯出記錄檔");
+			}
+		}
+	}
 }
